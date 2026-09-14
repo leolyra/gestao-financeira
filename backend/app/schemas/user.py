d0cd28b8d9725db2b553acba576d015b3061242f
leolyra <62756019+leolyra@users.noreply.github.com>
2026-09-14 14:52:48 -0300

    Update user.py

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,15 +1,15 @@
-from pydantic import BaseModel, str
+from pydantic import BaseModel, EmailStr
 from datetime import datetime
 
 class UserBase(BaseModel):
     name: str
-    email: str
+    email: EmailStr
 
 class UserCreate(UserBase):
     password: str
 
 class UserLogin(BaseModel):
-    email: str
+    email: EmailStr
     password: str
 
 class UserResponse(UserBase):

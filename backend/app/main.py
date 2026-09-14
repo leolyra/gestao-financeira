587a23a346d98a63f830146a43bdbd3b5d903660
leolyra <62756019+leolyra@users.noreply.github.com>
2026-09-14 12:01:35 -0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,9 +8,14 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title=settings.PROJECT_NAME, version="1.0.0")
 
+origins = [
+    "https://financeira.leo.lyra.nom.br",
+    "http://localhost:3000",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
